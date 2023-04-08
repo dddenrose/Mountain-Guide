628fb0cb051654edb283edfa00608e2a81d96b54
Chi Yang <dddenrose@gmail.com>
2023-04-08 12:47:50 +0800

    map update

--- src/component/Footer/Style.ts
+++ src/component/Footer/Style.ts
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 export const FooterWrapper = styled.div`
   width: 100%;
+  position: absolute;
   min-width: 100vw;
   background: burlywood;
   padding: 80px;
   box-sizing: border-box;
+  bottom: 0;
 `;
--- src/container/MapPage/Style.ts
+++ src/container/MapPage/Style.ts
@@ -1,10 +1,19 @@
 import styled from "styled-components";
 
 export const MapWrapper = styled.div`
-  width: 1000px;
-  
-  .cls-1, .cls-2 {
+  /* width: 1000px; */
+  margin-right: 80px;
+  svg {
+    border: 1px solid black;
+  }
+  .cls-1,
+  .cls-2 {
     fill: white;
     stroke: black;
   }
+`;
+
+export const MainWrapper = styled.div`
+  display: flex;
+  margin-top: 80px;
 `;
